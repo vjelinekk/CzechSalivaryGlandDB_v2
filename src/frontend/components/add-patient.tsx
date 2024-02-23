8f7e2f05f1449cbde8bdbfb41be14571a04c0429
Vojtěch Jelínek <jelinek@rtsoft.cz>
2024-02-23 19:59:27 +0100

    #11 První část komponenty

--- src/frontend/components/add-patient.tsx
+++ src/frontend/components/add-patient.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
 function AddPatient() {
-    return <div>AddPatient</div>
+    return (
+        <div id="patientAdd">
+            <ul>
+                <li>
+                    <button id="add-parotid-gland" className="mainButton">
+                        Příušní žláza
+                    </button>
+                </li>
+                <li>
+                    <button id="add-sublingual-gland" className="mainButton">
+                        Podjazyková žláza
+                    </button>
+                </li>
+                <li>
+                    <button id="add-submandibular-gland" className="mainButton">
+                        Podčelistní žláza
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default AddPatient
