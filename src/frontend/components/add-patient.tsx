f89bb527db23181f9f14ba1faf7cf048217369e5
Vojtěch Jelínek <jelinek@rtsoft.cz>
2024-02-24 18:54:56 +0100

    #11 Komponenta add-patient

--- src/frontend/components/add-patient.tsx
+++ src/frontend/components/add-patient.tsx
@@ -1,21 +1,48 @@
 import React from 'react'
+import { components } from '../constants'
 
-function AddPatient() {
+interface AddPatientProps {
+    setActiveComponent: React.Dispatch<React.SetStateAction<components>>
+}
+
+const AddPatient = (props: AddPatientProps) => {
+    const handleButtonClick = (componentName: components) => {
+        props.setActiveComponent(componentName)
+    }
+
     return (
         <div id="patientAdd">
             <ul>
                 <li>
-                    <button id="add-parotid-gland" className="mainButton">
+                    <button
+                        id="add-parotid-gland"
+                        className="mainButton"
+                        onClick={() =>
+                            handleButtonClick(components.parotidGlandForm)
+                        }
+                    >
                         Příušní žláza
                     </button>
                 </li>
                 <li>
-                    <button id="add-sublingual-gland" className="mainButton">
+                    <button
+                        id="add-sublingual-gland"
+                        className="mainButton"
+                        onClick={() =>
+                            handleButtonClick(components.sublingualGlandForm)
+                        }
+                    >
                         Podjazyková žláza
                     </button>
                 </li>
                 <li>
-                    <button id="add-submandibular-gland" className="mainButton">
+                    <button
+                        id="add-submandibular-gland"
+                        className="mainButton"
+                        onClick={() =>
+                            handleButtonClick(components.submandibularGlandForm)
+                        }
+                    >
                         Podčelistní žláza
                     </button>
                 </li>
---
+++ src/frontend/constants.ts
@@ -0,0 +1,9 @@
+export enum components {
+    patientsList = 'patients-list',
+    addPatient = 'add-patient',
+    studiesList = 'studies-list',
+    addStudy = 'add-study',
+    parotidGlandForm = 'parotid-gland-form',
+    sublingualGlandForm = 'sublingual-gland-form',
+    submandibularGlandForm = 'submandibular-gland-form',
+}
