b78f048565b532ee0d0d03a4a1bfd1be61974a6b
vjelinekk <vojtech.jelinekk@gmail.com>
2024-02-26 16:27:06 +0100

    #39 Komponenta personal-data

--- src/frontend/components/forms/conditional-checkboxes.tsx
+++ src/frontend/components/forms/conditional-checkboxes.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useSingleSelection } from '../..//hooks/useSingleSelection'
+import { ConditionalCheckboxOptionProps } from './conditional-checkbox-option'
 
 interface ConditionalCheckboxesProps {
     title: string
-    children: React.ReactNode
+    enableSingleSelect: boolean
+    children: React.ReactElement<ConditionalCheckboxOptionProps>[]
 }
 
 const ConditionalCheckboxes: React.FC<ConditionalCheckboxesProps> = ({
     title,
+    enableSingleSelect,
     children,
 }) => {
+    const { selectedOptions, handleCheckboxChange } =
+        useSingleSelection(enableSingleSelect)
+
     return (
         <div className="conditionalCheckboxDiv">
             <h3 className="conditionalCheckboxTitle">{title}</h3>
-            {children}
+            {children.map((child, index) => {
+                return React.cloneElement(child, {
+                    key: index,
+                    selected: selectedOptions.includes(child.props.label),
+                    onSelect: () => handleCheckboxChange(child.props.label),
+                })
+            })}
         </div>
     )
 }
