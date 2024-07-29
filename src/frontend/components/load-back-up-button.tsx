fb67be69eecd8e3677c933423a72beea9206bd36
vjelinekk <vojtech.jelinekk@gmail.com>
2024-07-30 00:57:08 +0200

    Restore warning message

--- src/frontend/components/load-back-up-button.tsx
+++ src/frontend/components/load-back-up-button.tsx
@@ -1,16 +1,43 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from '@mui/material'
 import React from 'react'
 
 const LoadBackUpButton: React.FC = () => {
-    const handleLoadBackUpClick = async () => {
+    const [openLoadBackUpDialog, setOpenLoadBackUpDialog] =
+        React.useState(false)
+
+    const handleLoadBackUpConfirm = async () => {
+        setOpenLoadBackUpDialog(false)
         await window.backUp.loadBackUp()
         window.location.reload()
     }
 
     return (
-        <button onClick={handleLoadBackUpClick}>
-            <img id="import" src="../img/restore.png" className="icon" />
-            Obnovit databázi
-        </button>
+        <>
+            <button onClick={() => setOpenLoadBackUpDialog(true)}>
+                <img id="import" src="../img/restore.png" className="icon" />
+                Obnovit databázi
+            </button>
+            <Dialog open={openLoadBackUpDialog}>
+                <DialogTitle>Obnovení databáze</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Databáze bude obnovena ze zálohy. Tento proces smaže
+                        veškerá data v databázi a nahradí je daty ze zálohy.
+                        Opravdu chcete pokračovat?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleLoadBackUpConfirm}>Rozumím</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
