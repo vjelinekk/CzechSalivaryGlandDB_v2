669fda9edc9d219ef158313e13d804a85c169335
vjelinekk <vojtech.jelinekk@gmail.com>
2025-05-28 18:45:01 +0200

    Odstranění konzole z deploye

--- src/main.ts
+++ src/main.ts
@@ -32,9 +32,9 @@ const createWindow = (): void => {
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
     // Open the DevTools.
-    // if (process.env.NODE_ENV === 'development') {
-    mainWindow.webContents.openDevTools()
-    // }
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools()
+    }
 }
 
 // This method will be called when Electron has finished
